43be1f86b847614516ec1858e3625891eba6f7c0
Adrian Rocha <adrianrochacon@gmail.com>
2024-04-17 15:40:41 -0600

    Mad cmabios

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb+srv://Adriancraft10:adrianrocha10@cluster0.j50qiec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://mern-crud-auth-1d7c.onrender.com:5174";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://mern-crud-auth-1d7c.onrender.com";
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ async function main() {
   try {
     await connectDB();
     app.listen(PORT);
-    console.log(`Listening on port https://mern-crud-auth-1d7c.onrender.com/:${PORT}`);
+    console.log(`Listening on port https://mern-crud-auth-1d7c.onrender.com`);
     console.log(`Environment: ${process.env.NODE_ENV}`)
   } catch (error) {
     console.error(error);
