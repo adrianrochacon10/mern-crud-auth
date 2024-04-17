1358d28bcf39817c28a6e087ecec8fbcc4ed46fe
Adrian Rocha <adrianrochacon@gmail.com>
2024-04-17 15:19:47 -0600

    variable

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000/api";
+export const API_URL = import.meta.env.VITE_API_URL|| "https://mern-crud-auth-1d7c.onrender.com/api";
